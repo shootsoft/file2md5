4a11ac773f02e7d672a08c3e84493c4a0241ae0f
shootsoft <shootsoft@qq.com>
2013-01-21 10:39:31 +0800

    Add 0.1
    Caculate MD5 & compare

---
+++ src/file2md5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace file2md5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = string.Empty;
+
+            if (args.Length > 1)
+            {
+                output = args[1];
+            }
+            else if (args.Length > 0)
+            {
+                output = args[0] + ".md5";
+            }
+            else
+            {
+                return;
+            }
+
+            try
+            {
+                if (File.Exists(args[0]))
+                {
+                    string md5 = GetMD5(args[0]);
+                    if (File.Exists(output))
+                    {
+                        string expected = TxtReader.Read(output).Trim().ToUpper();
+                        if (expected.Equals(md5))
+                        {
+                            Console.WriteLine("PASS!");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("FAIL!");
+                        }
+                        Console.WriteLine("Expected: " + expected);
+                        Console.WriteLine("Actual: " + md5);
+                        Console.Read();
+                    }
+                    else
+                    {
+                        File.WriteAllText(output, md5, Encoding.UTF8);
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.Read();
+            }
+        }
+
+        static string GetMD5(string file)
+        {
+            MD5 md5 = MD5.Create();
+            using (FileStream stream = new FileStream(file, FileMode.Open))
+            {
+                byte[] buff = md5.ComputeHash(stream);
+                StringBuilder result = new StringBuilder(buff.Length * 2);
+                for (int i = 0; i < buff.Length; i++)
+                    result.Append(buff[i].ToString("X2"));
+                return result.ToString();
+            }
+
+        }
+    }
+}
---
+++ src/file2md5/TxtReader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace file2md5
+{
+    /// <summary>
+    /// from http://blog.sina.com.cn/s/blog_6e51df7f0100so8z.html
+    /// </summary>
+    public class TxtReader
+    {
+        /// <summary>
+        /// 自动判断编码读取文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string Read(string path)
+        {
+            return File.ReadAllText(path, TxtReader.GetEncoding(path));
+        }
+
+        /// <summary>
+        /// 自动判断编码读取行文本
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string[] ReadAllLines(string path)
+        {
+            return File.ReadAllLines(path, TxtReader.GetEncoding(path));
+        }
+
+
+        ///<summary>
+        /// C#读取文件时自动判断编码函数
+        ///</summary>
+        ///<param name="path">需要判断编码方式文件的物理路径如"C:\file\file.txt"</param>
+        ///<returns></returns>
+        public static System.Text.Encoding GetEncoding(string path)
+        {
+            FileStream fs =new FileStream(path, FileMode.Open,FileAccess.Read);
+            System.Text.Encoding r = GetEncoding(fs);
+            fs.Close();
+            return r;
+        }
+
+        public static System.Text.Encoding GetEncoding(FileStream fs)
+        {
+            BinaryReader r =new BinaryReader(fs,System.Text.Encoding.Default);
+            byte[] ss = r.ReadBytes(4);
+            r.Close();
+            //编码类型 Coding=编码类型.ASCII;   
+            if (ss[0] <=0xEF)
+            {
+                if (ss[0] ==0xEF&& ss[1] ==0xBB&& ss[2] ==0xBF)
+                {
+                    return System.Text.Encoding.UTF8;
+                }
+                else if (ss[0] ==0xFE&& ss[1] ==0xFF)
+                {
+                    return System.Text.Encoding.BigEndianUnicode;
+                }
+                else if (ss[0] ==0xFF&& ss[1] ==0xFE)
+                {
+                    return System.Text.Encoding.Unicode;
+                }
+                else
+                {
+                    return System.Text.Encoding.Default;
+                }
+            }
+            else
+            {
+                return System.Text.Encoding.Default;
+            }
+        }
+    }
+}
